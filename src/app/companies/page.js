a9b97c6c0a9c18f46876856d35296849dc54eeb9
kishan sangani <202001265@daiict.ac.in>
2023-10-18 13:37:50 +0530

    kishan

--- src/app/companies/page.js
+++ src/app/companies/page.js
@@ -1,26 +1,40 @@
-import Header from "@/components/Header"
+import Header from "@/components/Header";
+import Card from "@/components/Company_card";
 
 const page = () => {
-    return (
-        <div style={{
-            width: '100%',
-            height: '100%',
-            fontSize: '1.5em',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            padding: '32px',
-            fontWeight: 'bold'
-        }}>
-            <div style={{
-                textAlign: 'center'
-            }}>
-                Companies feature coming soon...
-            </div>
-        </div>
-    )
-}
+  return (
+    // <div
+    //   style={{
+    //     width: "100%",
+    //     height: "100%",
+    //     fontSize: "1.5em",
+    //     display: "flex",
+    //     alignItems: "center",
+    //     justifyContent: "center",
+    //     flexDirection: "column",
+    //     justifyContent: "center",
+    //     padding: "32px",
+    //     fontWeight: "bold",
+    //   }}
+    // >
+    <div
+      style={{
+        margin: "50px",
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center",
+      }}
+    >
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+    //</div>
+  );
+};
 
-export default page 
+export default page;
