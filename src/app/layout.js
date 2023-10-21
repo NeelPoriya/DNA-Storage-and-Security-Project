4d51836625bc226e3ebac41c18e3f899a5d991de
NeelPoriya <neelporiya1309@gmail.com>
2023-10-21 16:55:06 +0530

    added google sigin & logout feature - frontend

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import NavBar from '@/components/NavBar'
 import './globals.css'
 import { Inter, Raleway } from 'next/font/google'
 import { ThemeProvider, createTheme } from '@mui/material'
+import { SessionProvider } from 'next-auth/react';
 
 const inter = Inter({ subsets: ['latin'] })
 const raleway = Raleway({ subsets: ['latin'] })
@@ -15,17 +16,19 @@ const theme = createTheme({
 
 export default function RootLayout({ children }) {
   return (
-    <ThemeProvider theme={theme}>
-      <html lang="en">
-        <body className={`${inter.className} ${raleway.className}`}>
-          <div className={'page'}>
-            <NavBar />
-            <div className={'content'}>
-              {children}
+    <SessionProvider>
+      <ThemeProvider theme={theme}>
+        <html lang="en">
+          <body className={`${inter.className} ${raleway.className}`}>
+            <div className={'page'}>
+              <NavBar />
+              <div className={'content'}>
+                {children}
+              </div>
             </div>
-          </div>
-        </body>
-      </html>
-    </ThemeProvider>
+          </body>
+        </html>
+      </ThemeProvider>
+    </SessionProvider>
   )
 }
